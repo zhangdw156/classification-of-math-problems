b9029574b1fdfb125b608dfbe09b78a7f6feb11d
h20 <h20@server>
2025-06-22 01:30:27 +0000

    update

--- predict.sh
+++ predict.sh
@@ -1,5 +1,5 @@
 python predict.py \
 --model_path "/data/download-model/DeepSeek-R1-0528-Qwen3-8B" \
---lora_path "finetuned_model" \
+--lora_path "checkpoints/checkpoint-step-16200" \
 --input_file "data/train.csv" \
 --output_file "submission.csv"
